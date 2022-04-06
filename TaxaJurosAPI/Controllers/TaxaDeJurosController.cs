dcca6e9516aea334370b53282c7066d298fcba1d
Taniguchi <tani_sales@hotmail.com>
2022-04-06 13:14:36 -0300

    Add project files.

---
+++ TaxaJurosAPI/Controllers/TaxaDeJurosController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaxaJurosAPI.Models;
+
+namespace TaxaJurosAPI.Controllers
+{
+
+    [ApiController]
+    public class TaxaDeJurosController : ControllerBase
+    {
+
+        /// <summary>
+        /// Taxa de Juros
+        /// </summary>
+        /// <param name="Taxa de Juros"></param>
+        /// <returns>Retorna <see cref="decimal"/></returns>
+        /// <response code="200">Retorna o valor da taxa de juros</response>
+        /// <response code="400">Formato de envio invalido</response>
+        /// <response code="401">Não autorizado</response>
+        /// <response code="403">Não permitido</response>
+        /// <response code="404">Não foi encontrado</response>
+        /// <response code="500">Erro de acesso ao servidor</response>
+        [HttpGet("taxaJuros")]
+        public ActionResult ObterTaxaDeJuros()
+        {
+            return Ok(new TaxaJuros().valor);
+        }
+    }
+}
